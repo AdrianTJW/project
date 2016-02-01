ee0f5a4c78f70911e10bb530a6cfbaee29e20e64
ytyeoh <ytyeoh@me.com>
2016-02-01 16:44:42 +0800

    pull request

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :hosts
-  devise_for :users
   resources :jobs
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   get 'home/index'
