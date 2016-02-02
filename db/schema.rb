4e4726c439f8334873ab5f0cc8d21ec8ca825394
andrewclk <andrewcheems@gmail.com>
2016-02-02 10:12:49 +0800

    beautified thing

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20160201090111) do
-=======
-ActiveRecord::Schema.define(version: 20160201095336) do
->>>>>>> master
+ActiveRecord::Schema.define(version: 20160202020344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -124,6 +120,7 @@ ActiveRecord::Schema.define(version: 20160201095336) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
