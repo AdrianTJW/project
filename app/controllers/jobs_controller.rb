5a5a67eed970c448b0dae7a461c1604f5df5135c
andrewclk <andrewcheems@gmail.com>
2016-02-03 12:20:49 +0800

    done homepage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -14,11 +14,13 @@ class JobsController < ApplicationController
 
     if params[:query].present?
       @jobs = Job.search(params[:query], page: params[:page])
+
     else
       # @jobs = Job.all.page params[:page]
       @jobs = Job.all
+      @jobs= @jobs.paginate(per_page: 3, page: params[:page])
     end
-    @jobs= @jobs.paginate(per_page: 3, page: params[:page])
+
   end
 
   def my_index
