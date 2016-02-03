b204eac9e5c1af41ff3d764f9f60615df1899e81
sooyang <sooyang.tan@gmail.com>
2016-02-03 15:46:31 +0800

    jq added

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,7 +4,7 @@ geocoded_by :address
 reverse_geocoded_by :latitude, :longitude do |obj,results|
   if geo = results.first
     obj.city    = geo.city
-    obj.zipcode = geo.postal_code
+    #obj.zipcode = geo.postal_code
     obj.country = geo.country_code
   end
 end
