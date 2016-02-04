1656cc31a0427908971c4b7e94229a14d65f4e5c
sooyang <sooyang.tan@gmail.com>
2016-02-04 17:26:47 +0800

    Job form update

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,13 +6,12 @@ class JobsController < ApplicationController
   # GET /jobs.json
   def index
     @jobs = Job.all
-    @hosts = Host.all
-
-        
+      
      @hash = Gmaps4rails.build_markers(@jobs) do |job, marker|
        marker.lat job.latitude
        marker.lng job.longitude
-       marker.infowindow [job.company_name, job.address]
+       # byebug
+       marker.infowindow job.company_name+ "<br>"+job.address+ '<br><img src=' + job.images[0].to_s+'>'
     end
 
     if params[:query].present?
